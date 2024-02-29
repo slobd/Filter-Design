7a4e1bf2fab75eda3a23b02b3d565eb53904912a
ever711 <talentdev725@gmail.com>
2024-02-29 09:38:53 -0800

    refresh type

--- frontend/utils/types.ts
+++ frontend/utils/types.ts
@@ -1,47 +1,5 @@
-export type NameType = {
-  firstName: string;
-  lastName: string;
-}
-
 export type OptionType = {
   value: string;
   label: string;
-}
-
-export type SignupType = {
-  email: string,
-  password: string
-}
-
-export type ProfileType = {
-  name: NameType,
-  email: string,
-  country: OptionType | undefined,
-  state: OptionType | undefined,
-  city: OptionType | undefined,
-  postalCode: string | "",
-  address: string | "",
-  phone: string | "",
-  bio: string | "",
-}
-
-export type ReceiptType = {
-  id: number | null,
-  user_id: string | "",
-  from: string | "",
-  logo: string | ""
-  name: string | "",
-  address: string | "",
-  link: string | "",
-  date: string | "",
-  time: string | "",
-  currency: string | "",
-  amount: number | "",
-}
-
-export type EmailType = {
-  id: number | null,
-  user_id: number | null,
-  email: string | ""
 }
 
